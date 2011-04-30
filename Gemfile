a2a7229f55250d366589cafcc7ce0392435592be
Fabio Costa <fabio.costa@deltasigma.com.br>
2011-04-30 11:21:59 -0300

    Commit inicial...

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'sqlite3'
+
+group :development do
+  gem 'rspec-rails' # , '2.5.0'
+end
+
+group :test do
+  gem 'rspec'
+  gem 'webrat'
+end
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+# group :development, :test do
+#   gem 'webrat'
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8a15bc22f80ad76e4eaaa170447d0e7d49f7b6b1bfbc040bfd167961d16824117856346f89d1dc2e8d7660b84540ea5b618ce46f7558e7122c6d2b59115d783d'
